1a45467b958b5e58c755ef3a57e2f46fce8e04e0
Siddharth Sawant <sid.sawant@kellogg.northwestern.edu>
2023-04-21 18:56:37 +0000

    Ruby commit1

--- code-along/1-data.rb
+++ code-along/1-data.rb
@@ -5,15 +5,31 @@
 # In Ruby, there are different *types* of data:
 
 # Numbers
+#puts 5
+#puts "Hello World!"
+#puts 7*(2+2)
+#puts 7.0/2.0
 
 # Perform simple math with numbers
 
 # Strings
+#puts "na jaane kyun...naa jaane kyun"
 
 # Combine strings together
 
 # Variables
+food = "tacos"
+#puts food
+
+#x = 5 
+#y = 7
+#d = x*y 
+#puts d 
 
 # Combine strings and variables
+#puts "Tacos" + " are awesome"
+#puts "tacos" + 3
+
+#puts "#{x + y} + tacos"
 
 # String manipulation
